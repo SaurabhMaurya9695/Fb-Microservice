eba932bdb2328ccbe670c4bc9edf035dcca92147
Saurabh Maurya <sm0107870@gmail.com>
2023-12-01 02:51:04 +0530

    added GlobalException in FR

---
+++ FriendRequest Microservice/src/main/java/com/request/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.user.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException() {
+        super("Resource Not Found Exception!!");
+    }
+
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
