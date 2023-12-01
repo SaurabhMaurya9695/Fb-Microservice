7e51b26f00078fc4fc488925e8183309657eab4a
Saurabh Maurya <sm0107870@gmail.com>
2023-12-02 01:11:50 +0530

    secure ApiGateway by adding AuthenticationFilter

---
+++ ApiGateway/src/main/java/com/gateway/gateway/AuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.gateway.gateway;
+
+import com.gateway.util.JwtUtil;
+import com.gateway.validator.RouteValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+
+    private Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                //header contains token or not
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    logger.info("Token Not Present ");
+                    throw new RuntimeException("MISSING AUTHORIZATION");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                try {
+                    logger.info("Token Present ");
+                    jwtUtil.validateToken(authHeader);
+                    logger.info("validation successfully Done ");
+                } catch (Exception e) {
+                    throw new RuntimeException("UN-AUTHORIZATION ACCESS TO APPLICATION");
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+
+    }
+
+}
