be2afffb58e5894598b08f9c53f683e8f4cf9d10
Saurabh Maurya <sm0107870@gmail.com>
2023-12-01 14:26:57 +0530

    Enable DiscoveryClient

--- PostMicroservices/src/main/java/com/post/PostMicroservicesApplication.java
+++ PostMicroservices/src/main/java/com/post/PostMicroservicesApplication.java
@@ -2,8 +2,10 @@ package com.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // byname now we can call an api
 public class PostMicroservicesApplication {
 
     public static void main(String[] args) {
