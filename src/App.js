cdb7bf4794f65f9c30c14765135d5046cf0b9aa2
Saurabh Maurya <sm0107870@gmail.com>
2023-12-07 22:03:26 +0530

    install redux and done with toggle mode

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './Pages/login';
 import Logout from './Pages/logout';
 import Home from './Pages/home';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import ChangeThemeProvider from  "./Context/theme.provider"
+import { useSelector } from 'react-redux';
+
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/logout" element={<Logout/>} />
-      </Routes>
-    </BrowserRouter>
+    <ChangeThemeProvider >
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/logout" element={<Logout/>} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </ChangeThemeProvider>
   );
 }
 
