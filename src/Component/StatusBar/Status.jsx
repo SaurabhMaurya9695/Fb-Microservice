e598135a0ade854d7e4db79059fa46a8f1c2872c
Saurabh Maurya <sm0107870@gmail.com>
2023-12-09 12:51:09 +0530

    done with status bar

---
+++ src/Component/StatusBar/Status.jsx
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material'
+import React from 'react'
+
+const Status = () => {
+  return (
+    <div>
+      <Paper className='statusbar_status'/>
+    </div>
+  )
+}
+
+export default Status
---
+++ src/Component/StatusBar/StatusBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Status from '../StatusBar/Status'
+import "../StatusBar/status.css";
+
+const StatusBar = () => {
+  return (
+    <div className='status_container'>
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+      <Status />
+    </div>
+  )
+}
+
+export default StatusBar
