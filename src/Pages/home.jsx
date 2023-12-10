3e1cd1fbd6f60ad560f8c8aa8831ae2fcf6d52e8
Saurabh Maurya <sm0107870@gmail.com>
2023-12-10 13:25:00 +0530

    ui done till middle part

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import Navbar from "../Component/Navbar"
+import { Row , Col } from 'react-bootstrap'
+import LeftSideBar from '../Component/LeftSideBar'
+import MiddleBar from '../Component/MiddleBar'
 const Home = () => {
   return (
     <div>
-      <h2>This is Home Page</h2>
+      <Navbar/>
+      <div className="container-fluid" >
+        <Row>
+          <Col md={3} >
+            {<LeftSideBar/>}
+          </Col>
+          <Col md={6} className='middleContainer'>
+            <h1>{<MiddleBar/>}</h1>
+          </Col>
+          <Col md={3}>
+            <h1>Right Side</h1>
+          </Col>
+        </Row>
+      </div>
     </div>
   )
 }
