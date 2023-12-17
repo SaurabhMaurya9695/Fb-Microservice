a67a64a17b2da1f55d9b73bd31c5f0bce363a010
Saurabh Maurya <sm0107870@gmail.com>
2023-12-17 19:08:51 +0530

    added three extra button for createPost

--- src/Component/UploadSection/UploadSection.jsx
+++ src/Component/UploadSection/UploadSection.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./upload.css";
 import { Avatar, Button, Divider, Paper, styled } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import SendIcon from '@mui/icons-material/Send';
 import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import { useSelector } from 'react-redux';
+import { createPost } from '../../Service/post.service';
 
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
@@ -19,33 +21,71 @@ const VisuallyHiddenInput = styled('input')({
 
 
 const UploadSection = () => {
+
+  const [post , setPost] = useState({
+    userId:"",
+    image :"",
+    description:"",
+  });
+
+  const user = useSelector((state) => state.user);
+
+  const sendPost = (event) =>{
+    event.preventDefault();
+    post.userId = user.userId;
+    console.log(post);
+    createPost(post).then((data)=>{
+      console.log(data);
+    }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
   return (
     <div>
       <Paper className='upload_container'>
         <div className='upload_top'>
           <div>
-            <Avatar className='upload_image' src='https://scontent.fknu1-5.fna.fbcdn.net/v/t39.30808-1/387684188_3540021242932260_4946501197515680151_n.jpg?stp=dst-jpg_p148x148&_nc_cat=106&ccb=1-7&_nc_sid=4da83f&_nc_ohc=dXZlwmmT--gAX_tKz5D&_nc_ht=scontent.fknu1-5.fna&oh=00_AfBTI1XJZ1izSU2wXLNuQRSOuo8AM-ltFUDb6TJWqzS4iA&oe=657822CF'/>
+            <Avatar className='upload_image' src={user.image}/>
           </div>
           <div>
-            <input className='upload_text' type='text' placeholder="what's on your mind ?"/>
+            <input className='upload_text' type='text' placeholder="what's on your mind ?"
+            onChange={(event) => {
+              setPost({
+                ...post,
+                description: event.target.value
+              })
+            }}/>
           </div>
         </div>
         <Divider sx={{color:"white"}} />
         <div className="upload_bottom">
           <div>
-            <Button component="label"  startIcon={<CloudUploadIcon />} size='medium'>
+            <Button component="label"  startIcon={<CloudUploadIcon />} size='medium'
+            onChange={(event) => {
+              setPost({
+                ...post,
+                image: event.target.value
+              })
+            }} >
               Image
               <VisuallyHiddenInput type="file" />
             </Button>
           </div>
           <div>
-            <Button component="label"  startIcon={<OndemandVideoIcon />} size='medium'>
+            <Button component="label"  startIcon={<OndemandVideoIcon />} size='medium'
+            onChange={(event) => {
+              setPost({
+                ...post,
+                image: event.target.value
+              })
+            }} >
               Video
               <VisuallyHiddenInput type="file" />
             </Button>
           </div>
           <div >
-            <Button  endIcon={<SendIcon />}>
+            <Button  endIcon={<SendIcon />} onClick={sendPost}>
               Upload Post
             </Button>
           </div>
