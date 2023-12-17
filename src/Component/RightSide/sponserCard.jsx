999d5e90df85198e455d6d9fd17796d4a2026766
Saurabh Maurya <sm0107870@gmail.com>
2023-12-17 19:33:57 +0530

    done with userImage in sponserCard

--- src/Component/RightSide/sponserCard.jsx
+++ src/Component/RightSide/sponserCard.jsx
@@ -1,8 +1,13 @@
 import { Paper, Typography } from '@mui/material';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './sponser.css'
+import { useSelector } from 'react-redux';
+import { getUserImage } from '../../Service/user.service';
+import { USER_BASE_URL } from '../../Service/helper.service';
 
 const SponserCard = () => {
+
+  const user = useSelector((state)=>state.user);
   return (
     <div >
       <Paper className='sponserCard_container'>
@@ -13,8 +18,8 @@ const SponserCard = () => {
       </div>
       <div className='sponser_image'>
         <img alt='noimage.jpeg'
-         src='https://scontent.fknu1-6.fna.fbcdn.net/v/t39.30808-6/404652825_3567987516802299_2413690461665923420_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=3635dc&_nc_ohc=BupQMJp7QusAX8-9CIV&_nc_ht=scontent.fknu1-6.fna&oh=00_AfC6__sHaznylPIlaY1F3zLUDdTrYRP4aI4cn1tdRamVBQ&oe=657A1305' 
-         width={250} height={200} className='image_src' />
+         src={USER_BASE_URL+"/users/image/"+user.userId}
+         width={250} height={250} className='image_src mt-2' />
       </div>
       <div className='sponser_text mt-3 ms-3'>
         <h6 className='sponser_text_css'> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus, cum.</h6>
